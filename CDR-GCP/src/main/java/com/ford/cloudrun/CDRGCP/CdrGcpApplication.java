c4569ec412b9b91d86741528cd07d036ca4634b6
RPOTHURA <rpothura@ford.com>
2023-02-18 10:19:36 -0600

    Initial Project

---
+++ CDR-GCP/src/main/java/com/ford/cloudrun/CDRGCP/CdrGcpApplication.java
@@ -0,0 +1,13 @@
+package com.ford.cloudrun.CDRGCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdrGcpApplication.class, args);
+	}
+
+}
---
+++ CDR-GCP/src/test/java/com/ford/cloudrun/CDRGCP/CdrGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.cloudrun.CDRGCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
